26a9b903ec4818a5c0f572438d29130f7e637309
Arjun Gadhia <arjungadhia@gmail.com>
2014-06-24 12:34:21 +0100

    Initial workingness! Need to test et al

--- src/js/Following.js
+++ src/js/Following.js
@@ -1,44 +1,108 @@
 'use strict';
 
 var jsonp = require('./lib/jsonp');
+var _ = require('lodash');
+var BrowserStore = require('./lib/BrowserStore');
+var event = require('./lib/event');
+var storage = new BrowserStore(localStorage);
+
 
 function Following(userId) {
 	this.userId = userId;
-	this.online = false;
-	this.entities = [];
+	this.entities = JSON.parse(storage.get('oFollowUserCache-' + this.userId)) || [];
+	this.fromStorage = true;
 }
 
 Following.prototype.set = function(data) {
 	if(data.status === 'success' && data.taxonomies) {
-		this.online = true;
-		this.entities = data.taxonomies;
+		var client = this.entities;
+		var server = data.taxonomies;
+		this.entities = server;
+		if(this.fromStorage) {
+			this.sync(client, server);
+		}
+		this.fromStorage = false;
+		this.save();
+	} else {
+		this.save();
+		this.fromStorage = true;
 	}
+	event.dispatch('oFollow.userPreferencesLoaded', this.entities);
 }
 
 Following.prototype.get = function() {
 	var self = this;
 	var url = 'http://personalisation.ft.com/follow/getFollowingIds?userId=' + this.userId;
 	jsonp.get(url, 'oFollowGetCallback', function(data) {
-		self.set(data)
+		self.set(data);
 	});
 }
 
 
+//TODO: refactor!
+function isInList(list, toCheck) {
+	return (_.filter(list, function(item) { return item.id === toCheck.id }).length > 0);
+}
+
+Following.prototype.sync = function(client, server) {
+	var self = this;
+	var clientOnly = _.filter(client, function(item) {
+		return !isInList(server, item);
+	});
+	var serverOnly = _.filter(server, function(item) {
+		return !isInList(client, item);
+	});
+
+	clientOnly.forEach(function(entity) {
+		self.start(entity);
+	});
+
+	serverOnly.forEach(function(entity) {
+		self.stop(entity);
+	})
+};
+
+Following.prototype.addEntity = function(entity) {
+	this.entities.push(entity);
+}
+Following.prototype.removeEntity = function(entity) {
+	_.remove(this.entities, {'id':entity.id});
+}
+
+Following.prototype.save = function() {
+	if(this.entities && this.entities.length) {
+		storage.put('oFollowUserCache-'+this.userId, JSON.stringify(this.entities));
+	}
+}
+Following.prototype.clear = function() {
+	storage.delete('oFollowUserCache-'+this.userId);
+}
+
 Following.prototype.start = function(entity) {
 	if(!(this.userId && entity.id && entity.name)) return;
+	var self = this;
 	var url = 'http://personalisation.ft.com/follow/update?userId=' + 
 			this.userId + '&type=authors&name=' +
 			entity.name + '&id=' +
 			entity.id;
-		jsonp.get(url, 'oFollowStartCallback');
+	this.addEntity(entity);
+	jsonp.get(url, 'oFollowStartCallback', function(data) {
+		self.set(data);
+	});
 }
 
-Following.prototype.stop = function(entity, userId) {
+Following.prototype.stop = function(entity) {
 	if(!(this.userId && entity.id && entity.name)) return;
+	var self = this;
 	var url = 'http://personalisation.ft.com/follow/stopFollowing?userId=' + 
 			this.userId + '&type=authors&id='+
 			entity.id;
-		jsonp.get(url, 'oFollowStopCallback');
-}
+	this.removeEntity(entity);
+	jsonp.get(url, 'oFollowStopCallback', function(data) {
+		self.set(data);
+	});
+};
+
+
 
 module.exports = Following;
--- tests/Following.test.js
+++ tests/Following.test.js
@@ -1,21 +1,24 @@
 var Following = require('../src/js/Following'),
 		jsonp =  require('../src/js/lib/jsonp.js');
 
+var following;
 
-describe('Following', function() {
+beforeEach(function() {
+	following = new Following('userId');
+});
+
+describe('Getting the initial model', function() {
 
 
 	it('get() makes a call to fetch the latest following data for a user', function() {
 		var getSpy = spyOn(jsonp, 'get');
-		var following = new Following('testUser');
 		following.get();
-		var url = 'http://personalisation.ft.com/follow/getFollowingIds?userId=testUser';
+		var url = 'http://personalisation.ft.com/follow/getFollowingIds?userId=userId';
 		expect(getSpy).toHaveBeenCalledWith(url, 'oFollowGetCallback', jasmine.any(Function));
 	})
 
 	it('set() updates the model based on the data recieved', function() {
 
-		var following = new Following();
 		var data = {"status":"success", "taxonomies":[{"id":"Q0ItMDAwMDcxOA==-QXV0aG9ycw==","name":"Jack Farchy","type":"authors"}]};
 		following.set(data);
 		expect(following.entities[0]).toBe(data.taxonomies[0]);
@@ -24,13 +27,15 @@ describe('Following', function() {
 
 
 	it('set() only works if the data is correct', function() {
-		var following = new Following();
 		var data = {"blah":[{"id":"Q0ItMDAwMDcxOA==-QXV0aG9ycw==","name":"Jack Farchy","type":"authors"}]};
 		following.set(data);
 		expect(following.entities.length).toBe(0);
 		expect(following.online).not.toBeTruthy();
 	});
 
+});
+
+describe('Updating the model', function() {
 
 	it('a user can follow an author', function() {
 		var callback = function() {}
@@ -39,9 +44,8 @@ describe('Following', function() {
 			name: 'Arjun',
 			id: 'arjunId'
 		}
-		var following = new Following();
-
-		following.start(entity, 'userId');
+		following.online = true;
+		following.start(entity);
 		var expectedUrl = 'http://personalisation.ft.com/follow/update?userId=' + 
 			'userId&type=authors&name=Arjun&id=arjunId';
 
@@ -56,12 +60,63 @@ describe('Following', function() {
 			name: 'Arjun',
 			id: 'arjunId'
 		}
-		var following = new Following();
-
-		following.stop(entity, 'userId');
+		following.online = true;
+		following.stop(entity);
 		var expectedUrl = 'http://personalisation.ft.com/follow/stopFollowing?userId=' + 
 			'userId&type=authors&id=arjunId';
 
 		expect(getSpy).toHaveBeenCalledWith(expectedUrl,'oFollowStopCallback');
 	});
 });
+
+
+describe('Keeping the client and server in sync', function() {
+	it('Makes any pending follow requests', function() {
+		var following = new Following('testId');
+		following.entities = [
+			{"id":"1","name":"Saved author","type":"authors"},
+			{"id":"2","name":"Unsaved Author","type":"authors"}
+		]
+		var server = [following.entities[0]];
+		var startSpy = spyOn(following, 'start');
+		following.sync(server);
+		expect(startSpy).toHaveBeenCalledWith(following.entities[1]);
+	});
+
+
+	it('Makes any pending unfollow requests', function() {
+		var following = new Following('testId');
+		var server = [
+			{"id":"1","name":"Unfollowed author","type":"authors"},
+			{"id":"2","name":"Saved Author","type":"authors"}
+		]
+		following.entities = [server[1]];
+		var stopSpy = spyOn(following, 'stop');
+		following.sync(server);
+		expect(stopSpy).toHaveBeenCalledWith(server[0]);
+	});
+});
+
+describe('Adding and removing entity to client list', function() {
+	it('adds entity and saves', function() {
+		following.entities = [
+			{"id":"1","name":"Saved author","type":"authors"},
+			{"id":"2","name":"Another Author","type":"authors"}
+		]		
+		var newAuthor = {"id":"3","name":"New Author","type":"authors"}
+		following.addEntity(newAuthor);
+		expect(following.entities.length).toBe(3);
+		expect(following.entities[2].name).toBe('New Author');
+	});
+
+	it('removes entity and saves', function() {
+		following.entities = [
+			{"id":"1","name":"Saved author","type":"authors"},
+			{"id":"2","name":"Another Author","type":"authors"}
+		]		
+		var removeAuthor = {"id":"1","name":"Saved Author","type":"authors"}
+		following.removeEntity(removeAuthor);
+		expect(following.entities.length).toBe(1);
+		expect(following.entities[0].name).toBe('Another Author');
+	});
+})
