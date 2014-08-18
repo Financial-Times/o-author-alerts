713200a0c79654df37a4e0a8131bdfcbf06c52d7
Arjun Gadhia <arjungadhia@gmail.com>
2014-08-18 14:24:47 +0100

    Bring back oDom and cleanup

--- src/js/lib/eventHelper.js
+++ src/js/lib/eventHelper.js
@@ -10,16 +10,3 @@ exports.dispatch = function(name, data, rootEl) {
         rootEl.dispatchEvent(event);
     }
 };
-
-exports.listen = function(name, data, rootEl) {
-	'use strict';
-	rootEl = rootEl || document.body;
-  if (document.createEvent && rootEl.dispatchEvent) {
-        var event = document.createEvent('Event');
-        event.initEvent(name, true, true);
-        if (data) {
-            event.detail = data;
-        }
-        rootEl.dispatchEvent(event);
-    }
-};
